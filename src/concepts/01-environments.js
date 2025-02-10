e1f9624038c1fe1d586969733ceb60666f459197
Yancy marine <yanci.marine@gmail.com>
2025-02-10 18:27:06 -0400

    lunes tarde

---
+++ src/concepts/01-environments.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const environmentsComponent = (element) =>{
+      const html = `
+        variables:
+        `;
+        element.innerHTML = html;
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { environmentsComponent } from './concepts/01-environments'
+
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -13,12 +14,11 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+   
+  </div>`;
+console.log("hola mundo");
+/*setupCounter(document.querySelector('#counter'))*/
+const elemento = document.querySelector('.card');
+environmentsComponent(elemento);
